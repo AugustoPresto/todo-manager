8be19b94724fd5d44a922c617f633d0e68306cee
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-04-17 14:15:01 -0300

    added edit and delete actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # read all
   get 'tasks', to: 'tasks#index'
-  # read one
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-
+  
   # create task
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
+  
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # edit task
-  get 'task/:id/edit', to: 'task#edit'
-  patch 'task/:id', to: 'task#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
 
   # delete task
-  delete 'task/:id', to: 'task#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
